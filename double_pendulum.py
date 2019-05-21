bed62621b5e33c68ff23fb398ea759de8e80b421
Lam Ngo <ngobasonlam@gmail.com>
2019-05-21 12:21:40 -0400

    simple visualization added

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,50 +1,83 @@
 import getopt
 import os
 import sys
-
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+import double_pendulum_solver as dp
+import test_solver as ts
+import math
 
 def main():
-    g = 10
-    dt = 0.01
-    m = 1.0
-    theta = phi = 0.5
+    #default values
+    G = 9.8
     L = 1.0
+    M = 1.0
+    dt = 0.025
 
-    try:
-        
+    #time stepping range 0 -> 20 seconds, by dt. retyrns list
+    time_steps = np.arange(0.0, 20, dt)
 
-def print_usage():
-    """Prints usage instructions to stderr and exits."""
+    #default initial
+    theta = np.radians(120.0)
+    phi = np.radians(-10.0)
+    w_1 = np.radians(0.0)
+    w_2 = np.radians(0.0)
 
-    output = "Usage: %s [OPTIONS]\n\n" % os.path.basename(__file__)
-    output += "    -h, --help                    "
-    output += "prints instructions\n"
-    output += "    -v, --verbose                 "
-    output += "activates verbose mode\n"
-    output += "    -g, --gravity=ACCEL           "
-    output += "sets the gravitational acceleration\n"
-    output += "    -s, --time-step=STEP          "
-    output += "sets the simulation time step\n"
-    output += "    -m, --mass=mass      "
-    output += "sets the mass of each ball\n"
-    output += "    -t, --theta=theta,phi     "
-    output += "sets the initial angle of each ball\n"
-    output += "    -w, --omega=OMEGA1,OMEGA2     "
-    output += "sets the initial angular velocity of each ball\n"
-    output += "    -L, --rodlen=LEN      "
-    output += "sets the rod length for each ball\n"
-    output += "    -verlet     "
-    output += "using verlet method\n"
-    output += "     -rk4        "
-    output += "using fourth order runge-kutta\n"
-    sys.stderr.write(output)
-    sys.exit(0)
+    inputs = np.array([G, M, theta, phi, L, w_1, w_2])
+    double_pendulum_system = dp.DoublePendulum(*inputs)
 
-def print_error(errmsg):
-    """Prints an error message and exits with an error code (1)."""
+    thetas = []
+    phis = []
 
-    sys.stderr.write("Error: %s\n" % errmsg)
-    sys.exit(1)
+    thetas.append(theta)
+    phis.append(phi)
+
+    i = 1
+    #time stepping loop to create values
+    while (i < len(time_steps)):
+        double_pendulum_system.integrate_runge_kutta(dt)
+        thetas.append(double_pendulum_system.theta)
+        phis.append(double_pendulum_system.phi)
+        i += 1
+    
+    x_1 = []
+    y_1 = []
+    x_2 = [] 
+    y_2 = []
+
+    #calculate coords
+    x_1 = [L * math.sin(theta) for theta in thetas]
+    y_1 = [-L * math.cos(theta) for theta in thetas]
+    x_2 = [L*math.sin(phis[i]) + x_1[i] for i in range(len(thetas))]
+    y_2 = [-L*math.cos(phis[i]) + y_1[i] for i in range(len(thetas))]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2,2))
+    ax.set_aspect('equal')
+    ax.grid()
+
+    line, = ax.plot([], [], 'o-', lw=2)
+    time_template = 'time = %.2fs'
+    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+    def init():
+        line.set_data([], [])
+        time_text.set_text('')
+        return line, time_text
+
+    def animate(i):
+        thisx = [0, x_1[i], x_2[i]]
+        thisy = [0, y_1[i], y_2[i]]
+
+        line.set_data(thisx, thisy)
+        time_text.set_text(time_template % (i*dt))
+        return line, time_text   
+
+    interval = 1000 * dt
+    
+    ani = animation.FuncAnimation(fig, animate, range(0, len(thetas)), interval= interval, blit=True, init_func=init)
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- double_pendulum_solver.py
+++ double_pendulum_solver.py
@@ -20,9 +20,7 @@ class DoublePendulum:
         self.w_2 = w_2 # intial angular velocity ball 2
 
         self.nat_w = self.g/self.L
-
-        self.xi = [self.theta, self.w_1, self.phi, self.w_2] #y(0)
-    
+        
     def compute_right_hand_size(self, theta, w_1, phi, w_2):
         """
         theta - the angle of first ball
@@ -31,36 +29,28 @@ class DoublePendulum:
         w_2 - angular velocity of second ball
         """
 
-        g_numerator_1 = 2 * (self.nat_w)**2 ) * ( math.sin(theta) * math.cos(theta - phi) - math.sin(phi) )
-        g_numerator_2 = ((math.cos(theta - phi) * (w_2**2)) + 2 * (w_1**2)) * math.sin(theta - phi)
+        g_numerator_1 = 2.0 * ((self.nat_w) ) * ( math.sin(theta) * math.cos(theta - phi) - math.sin(phi) )
+        g_numerator_2 = ((math.cos(theta - phi) * (w_2**2.0)) + 2.0 * (w_1**2)) * math.sin(theta - phi)
         g_numerator = g_numerator_1 + g_numerator_2
         g_denominator = 1 + (math.sin(theta -phi))**2
         g = g_numerator / g_denominator 
 
-        f_numerator_1 = (self.nat_w ** 2) * ( math.sin(phi) * math.cos(theta - phi) - 2 * math.phi(theta))
-        f_numerator_2 = (w_2 ** 2 + w_1 ** 2 * math.cos(theta - phi)) * math.sin(theta - phi)
+        f_numerator_1 = (self.nat_w) * ( math.sin(phi) * math.cos(theta - phi) - 2.0 * math.sin(theta))
+        f_numerator_2 = ((w_2 ** 2) + (w_1 ** 2) * math.cos(theta - phi)) * math.sin(theta - phi)
         f_numerator = f_numerator_1 - f_numerator_2
-        f_denominator = 1 + (math.sin(theta - phi)) ** 2
+        f_denominator = 1 + (math.sin(theta - phi)) ** 2.0
         f = f_numerator / f_denominator
 
         return numpy.array([w_1, f, w_2, g])
+    
+    def get_coords(self):
+        x1 = L*math.sin(theta)
+        y1 = -L*math.cos(theta)
 
-    def integrate_verlet(self, dt):
-        #advance one time step
-        #verlet integration
-        #temp copies of current model's variables 
-        theta = self.theta
-        phi = self.phi
-        w_1 = self.w_1
-        w_2 = self.w_2
+        x2 = L*math.sin(phi) + x1
+        y2 = -L*math.cos(phi) + y1
 
-        #calculate y(k*dt)
-        self.xi = self.xi + self.compute_right_hand_size(theta, w_1, phi, w_2) * dt
-
-        self.theta = self.xi[0]
-        self.w_1 = self.xi[1]
-        self.phi = self.xi[2]
-        self.w_2 = self.xi[3]
+        return [x1, y1, x2, y2]
 
     def integrate_runge_kutta(self, dt):
         #advance one time step
@@ -69,16 +59,16 @@ class DoublePendulum:
         inputs = numpy.array([self.theta, self.w_1, self.phi, self.w_2])
 
         k_1 = self.compute_right_hand_size(*inputs)
-        k_2 = self.compute_right_hand_size(*(inputs + dt * k_1 / 2))
-        k_3 = self.compute_right_hand_size(*(inputs + dt * k_2 / 2))
+        k_2 = self.compute_right_hand_size(*(inputs + dt * k_1 * 0.5))
+        k_3 = self.compute_right_hand_size(*(inputs + dt * k_2 * 0.5))
         k_4 = self.compute_right_hand_size(*(inputs + dt * k_3 ))
 
-        self.xi = self.xi + (k_1 + 2*k_2 + 2*k_3 + k_4) * dt / 6
+        delta = (k_1 + 2*k_2 + 2*k_3 + k_4) * dt * 1.0 / 6.0
 
-        self.theta = self.xi[0]
-        self.w_1 = self.xi[1]
-        self.phi = self.xi[2]
-        self.w_2 = self.xi[3]
+        self.theta += delta[0]
+        self.w_1 += delta[1]
+        self.phi += delta[2]
+        self.w_2 += delta[3]
 
         
 
