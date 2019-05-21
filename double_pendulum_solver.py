834fb0632e65627bac9abdded2d67d3b04c912c4
tuanmp <40601073+tuanmp@users.noreply.github.com>
2019-05-21 22:50:55 +0700

    Update double_pendulum_solver.py
    Modification of Verlet

--- double_pendulum_solver.py
+++ double_pendulum_solver.py
@@ -9,6 +9,8 @@ class DoublePendulum:
         phi - intial angle of ball #2
         L - length of the rod for both balls
         """
+        #comment: We actually don't really need the masses of the pendula, as long as they are equal. An interesting feature of the simple 
+        #pendulum is that the angular frequency does not depend on the mass!
 
         self.g = g
         self.m = m
@@ -19,9 +21,18 @@ class DoublePendulum:
         self.w_1 = w_1 # initial angular velocity ball 1
         self.w_2 = w_2 # intial angular velocity ball 2
 
-        self.nat_w = self.g/self.L
+        self.nat_w = sqrt(self.g/self.L) #nat_w^2=g/L
 
         self.xi = [self.theta, self.w_1, self.phi, self.w_2] #y(0)
+        
+        #Define two-array objects for Verlet integration
+        self.Psi = [self.theta, self.phi]
+        self.Omega = [self.w_1, self.w_2]
+        
+        #Initial value for Psi((n-1)*dt)
+        theta_minus1 = self.theta - w_1*dt
+        phi_minus1 = self.theta - w_2*dt
+        self.Psi_minus1 = [theta_minus1, phi_minus1]
     
     def compute_right_hand_size(self, theta, w_1, phi, w_2):
         """
@@ -44,6 +55,24 @@ class DoublePendulum:
         f = f_numerator / f_denominator
 
         return numpy.array([w_1, f, w_2, g])
+    
+    def accelerations(self, theta, w_1, phi, w_2):
+        """Verlet is a second-order algorithm, which requires the second-order term in the Taylor expansion, namely the acceleration. 
+        Therefore, we better work with equations 2.2.7 and 2.2.8 in my note than equation 2.2.12"""
+        
+        g_numerator_1 = 2.0 * (self.nat_w)**2 ) * ( math.sin(theta) * math.cos(theta - phi) - math.sin(phi) )
+        g_numerator_2 = ((math.cos(theta - phi) * (w_2**2)) + 2.0 * (w_1**2)) * math.sin(theta - phi)
+        g_numerator = g_numerator_1 + g_numerator_2
+        g_denominator = 1.0 + (math.sin(theta -phi))**2
+        g = g_numerator / g_denominator 
+
+        f_numerator_1 = (self.nat_w ** 2) * ( math.sin(phi) * math.cos(theta - phi) - 2.0 * math.phi(theta))
+        f_numerator_2 = (w_2 ** 2 + w_1 ** 2 * math.cos(theta - phi)) * math.sin(theta - phi)
+        f_numerator = f_numerator_1 - f_numerator_2
+        f_denominator = 1.0 + (math.sin(theta - phi)) ** 2
+        f = f_numerator / f_denominator
+        
+        return numpy.array([f,g]) #This is practically identical to the previous method, only now we need a two-entry array
 
     def integrate_verlet(self, dt):
         #advance one time step
@@ -53,14 +82,19 @@ class DoublePendulum:
         phi = self.phi
         w_1 = self.w_1
         w_2 = self.w_2
-
+        clone_Psi = self.Psi
+  
         #calculate y(k*dt)
-        self.xi = self.xi + self.compute_right_hand_size(theta, w_1, phi, w_2) * dt
+        self.Psi = self.Psi*2 - self.Psi_minus1 + self.accelerations(theta, w_1, phi, w_2) * dt 
+        self.Omega = (self.Psi - clone_Psi)/dt
 
-        self.theta = self.xi[0]
-        self.w_1 = self.xi[1]
-        self.phi = self.xi[2]
-        self.w_2 = self.xi[3]
+        self.theta = self.Psi[0]
+        self.w_1 = self.Omega[0]
+        self.phi = self.Psi[1]
+        self.w_2 = self.Omega[1]
+        self.Psi_minus1 = clone_Psi
+        
+        
 
     def integrate_runge_kutta(self, dt):
         #advance one time step
